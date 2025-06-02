af37e1c4dea84d92773e4495a8a67501415db97d
Antonio Del Papa <antonio.delpapa@centropaghe.it>
2025-06-02 13:35:39 +0200

    Aggiorna .gitignore e aggiungi cartella js

---
+++ wwwroot/js/site.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('table.table-main, table.dataTable').DataTable({
+        // Opzioni globali, es. lingua italiana
+        "language": { "url": "//cdn.datatables.net/plug-ins/1.13.6/i18n/it-IT.json" }
+    });
+});
